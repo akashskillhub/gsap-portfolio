997b0912cc1b38d76bbf07a2d89e9d39626fbd24
akash <akash@gmail.com>
2023-03-30 16:31:28 +0530

    29-03-2023

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,13 @@ const navLink = document.querySelector(".nav-link")
 console.log("app running...")
 
 const tl = gsap.timeline()
+tl.from(".navbar", { width: 0, duration: 1 })
+tl.from(".nav-link", {
+    y: "100%",
+    opacity: 0,
+    stagger: 0.2,
+    duration: 1
+})
 tl.from(user, {
     y: "100vh",
     opacity: 0,
@@ -27,3 +34,20 @@ tl.from(desc, {
     x: "200vh",
     duration: 1
 })
+tl.from("#cta", {
+    scale: 3,
+    opacity: 0,
+    duration: 1
+})
+
+
+gsap.from(".circle", { scale: 0, repeat: -1, yoyo: true, duration: 0.6 })
+
+window.addEventListener("mousemove", function (event) {
+    const x = event.clientX
+    const y = event.clientY
+    gsap.to(".circle", 0.7, { x, y })
+    gsap.to(".tiny", 0.2, { x, y })
+}
+)
+
